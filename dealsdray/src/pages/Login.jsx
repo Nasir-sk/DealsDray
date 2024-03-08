24b50c500828e9d39f505e507085253c3008bbff
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-08 15:45:41 +0530

    create login integration and localStorage for user

--- dealsdray/src/pages/Login.jsx
+++ dealsdray/src/pages/Login.jsx
@@ -1,17 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function Login() {
     const [email, setEmail]=useState('');
     const [password, setPassword]=useState('');
+    const navigate = useNavigate();
+    // useEffect=(()=>{
+    //     const auth = localStorage.getItem('user');
+    //     if(auth){
+    //         navigate('/dashboard')
+    //     }
+    // })
 
-    useEffect(()=>{
-        console.log(email, password);
-    })
+    const handleLogin= async()=>{
+        console.warn(email, password);
+        let result = await fetch('http://localhost:4500/login',{
+            method:"POST",
+            body:JSON.stringify({email, password}),
+            headers:{'Content-Type':'application/json'}
+        });
+        result = await result.json();
+        console.warn(result);
+        if(result)
+        {
+            localStorage.setItem('user',JSON.stringify(result.user));
+            navigate('/dashboard')
+        }else{
+            alert("please enter correct details")
+        }
+
+    }
+
   return (
     <div className='login'>
         <h3>Login</h3>
         <input className="inputbox" placeholder="Enter Email" onChange={(e)=>setEmail(e.target.value)} value={email} />
         <input className="inputbox" placeholder="Enter Password" onChange={(e)=>setPassword(e.target.value)} value={password} />
-        <button className="appbutton"  type="button">Login</button>
+        <button className="appbutton" onClick={handleLogin} type="button">Login</button>
     </div>
   )
 }
