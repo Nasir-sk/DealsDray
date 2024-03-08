0364df1a4086c16a0ecaf35431094741c1ced0cf
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-08 10:15:22 +0530

    create pages and routes

--- dealsdray/src/App.jsx
+++ dealsdray/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import CreateEmp from './pages/CreateEmp';
+import EditEmp from './pages/EditEmp';
+import EmployeeList from './pages/EmployeeList';
+import Login from './pages/Login';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <div>
-        
-      </div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      
+      <Route path='/' element={<Home/>}/>
+      <Route path='/emp-list' element={<EmployeeList/>} />
+      <Route path='/create-emp' element={<CreateEmp/>} />
+      <Route path='/edit-emp' element={<EditEmp/>} />
+      <Route path='/logout' element={<h1> Logout Component</h1>} />  
+      <Route path='/dashboard' element={<Dashboard/>} />
+
+      <Route path='/login' element={<Login/>} />
+      
+    </Routes>
+    </BrowserRouter>
+
     
   )
 }
---
+++ dealsdray/src/pages/CreateEmp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreateEmp() {
+  return (
+    <div>CreateEmp</div>
+  )
+}
