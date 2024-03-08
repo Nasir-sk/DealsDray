2179122a1d5d0454dd63c4f72115de993b1d1d2c
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-08 12:45:29 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import config from './db/config.js'
+import User from './db/model/Users.js';
 const app = express();
 
 console.log("api is working!!");
