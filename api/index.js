a6e143cb16b0342bd6452ffe66ee6f86e8fd403e
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-03-08 14:46:39 +0530

    create login api

--- api/index.js
+++ api/index.js
@@ -2,8 +2,30 @@ import express from 'express';
 import mongoose from 'mongoose';
 import config from './db/config.js'
 import User from './db/model/Users.js';
+import cors from 'cors';
 const app = express();
-
+app.use(express.json());
+app.use(cors())
 console.log("api is working!!");
+
+app.post('/signup', async(req, resp)=>{
+    let user = new User(req.body);
+    let result = await user.save();
+    resp.send(result)
+})
+
+app.post('/login', async(req,resp)=>{
+    console.log(req.body);
+    if(req.body.password && req.body.email){
+        let user = await User.findOne(req.body)
+        if(user){
+            resp.send(user)
+        }else{
+            resp.send({result:"no user found"})
+        }
+    }else{
+        resp.send({result:"no user found"})
+    }
+})
 
 app.listen(4500);
